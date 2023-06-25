dc6cb7da0ba44ba5de0a6f481bc0a6e50cfe1779
butonG21 <73821573+butonG21@users.noreply.github.com>
2023-06-25 18:49:19 +0000

    correct typo

--- client.js
+++ client.js
@@ -27,7 +27,7 @@ function generateRandomString() {
     const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
     let randomString = '';
     for (let i = 0; i < 8; i++) {
-        const randomIndex = Math.floor(Math.random() * characters,length)
+        const randomIndex = Math.floor(Math.random() * characters.length)
         randomString += characters.charAt(randomIndex);
     }
     return randomString;
